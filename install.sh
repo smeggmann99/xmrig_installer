465037497a027f8cb647cc8dd1754c7ef5ad423e
smugg99 <dudak799@gmail.com>
2024-06-02 01:34:45 +0200

    added xmrig submodule

---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+	echo "This script must be run as root" 
+	exit 1
+fi
+
+do_shared_things() {
+	
+}
+
+install_centos8() {
+	echo "Building XMRig for CentOS 8..."
+
+	do_shared_things
+
+	sudo dnf install -y epel-release
+	sudo yum config-manager --set-enabled PowerTools
+	sudo dnf install -y git make cmake gcc gcc-c++ libstdc++-static automake libtool autoconf
+	
+	mkdir xmrig/build
+	cd xmrig/scripts && ./build_deps.sh && cd ../build
+
+	cmake .. -DXMRIG_DEPS=scripts/deps
+	make -j$(nproc)
+}
+
+install_ubuntu() {
+	echo "Building XMRig for Ubuntu..."
+}
+
+display_menu() {
+    clear
+    echo "Which system would you like to build XMRig for?"
+    echo "1. CentOS 8"
+    echo "2. Ubuntu"
+    echo "3. (Cancel)"
+}
+
+handle_input() {
+    read -rp "Enter your choice: " choice
+    case $choice in
+        1) install_centos8 ;;
+        2) install_ubuntu ;;
+        3) echo "Cancelling..."; exit;;
+        *) echo "Invalid choice. Please choose again.";;
+    esac
+}
+
+# Main function
+main() {
+    while true; do
+        display_menu
+        handle_input
+        read -rp "Press Enter to continue..."
+    done
+}
+
+# Start the script
+main
